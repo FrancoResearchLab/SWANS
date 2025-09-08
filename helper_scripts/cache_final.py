cb0631cbae450103d19c8691f31100ba1f7d0e53
beigelk <123108611+beigelk@users.noreply.github.com>
2025-09-08 12:11:47 -0400

    adding configs that are paths as exclusions to lowercasing in `get_config()`

--- helper_scripts/cache_final.py
+++ helper_scripts/cache_final.py
@@ -5,15 +5,23 @@ reference_type, reference = '', ''
 config = sys.argv[1]
 
 def get_config(search_config):
+
+	# configs to exclude from lowercasing
+	exclusion_list = (
+		'CLUSTER_ANNOTATION_FILE',
+		'USER_ANALYZED_SEURAT_OBJECT',
+		'FINAL_USER_GENE_FILE'
+	)
+
 	with open('configs/post_annotation_configs.yaml') as input_file:
 		for line in input_file.readlines():
 			line = line.lstrip()
 			if line.startswith(search_config):
 				reference = line.split(search_config)[1].replace('\n', '')
 				reference = reference.replace(':', '').lstrip().rstrip()
-				reference = reference.lower() # guarantees everything is lowercase
-				#if search_config.startswith('PROJECT'):
-				#	reference = reference.lower()
+				# if the config is not in the exclusion_list, lowercase it
+				if not line.startswith(exclusion_list):
+					reference = reference.lower() 
 	return(reference)
 
 selection = get_config(config)
